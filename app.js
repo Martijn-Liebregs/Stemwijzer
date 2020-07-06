75346097a4e5a8e512323c47f0d66160c539dc81
Martijn-Liebregs <55746417+Martijn-Liebregs@users.noreply.github.com>
2020-07-06 15:36:20 +0200

    Finished

--- app.js
+++ app.js
@@ -1,66 +1,63 @@
 let Stemwijzer = (function(){
 
 	// Some variables
-	let question 			= 29;
-	let party_vote_count 	= {};
-	let votes 				= [];
-	let weighted_questions 	= [];
+	let question = 0;
+	let party_vote_count = {};
+	let votes = [];
+	let weighted_questions = [];
 
-	// Get elements
-	let main 			= document.getElementById('main');
-	let title_div 		= document.getElementById('title');
-	let statement_div	= document.getElementById('statement');
-	let opinion_div 	= document.getElementById('opinion');
-	let weighted_div 	= document.getElementById('weighted_questions');
-
-	let start_btn 		= document.getElementById('start');
-	let vote_btn_pro 	= document.getElementById('vote_btn_pro');
+	// Get elements by id
+	let main = document.getElementById('main');
+	let title_div = document.getElementById('title');
+	let statement_div = document.getElementById('statement');
+	let opinion_div = document.getElementById('opinion');
+	let weighted_div = document.getElementById('weighted_questions');
+	// Get elements by id
+	let start_btn = document.getElementById('start');
+	let vote_btn_pro = document.getElementById('vote_btn_pro');
 	let vote_btn_contra = document.getElementById('vote_btn_contra');
-	let vote_btn_none 	= document.getElementById('none');
-	let vote_btn_skip 	= document.getElementById('skip');
-
-	let prev_btn 		= document.getElementById('previous');
-	let done_btn 		= document.getElementById('done');
-	let restart_btn 	= document.getElementById('restart')
-
-	let show_opinion			= document.getElementById('opinion');
-	let show_big_parties	 	= document.getElementById('b_parties');
-	let show_secular_parties 	= document.getElementById('s_parties');
+	let vote_btn_none = document.getElementById('none');
+	let vote_btn_skip = document.getElementById('skip');
+	// Get elements by id
+	let prev_btn = document.getElementById('previous');
+	let done_btn = document.getElementById('done');
+	let restart_btn = document.getElementById('restart')
+	// Get elements by id
+	let show_opinion = document.getElementById('opinion');
+	let show_big_parties = document.getElementById('b_parties');
+	let show_secular_parties = document.getElementById('s_parties');
 
 	// Set element displays
-	vote_btn_pro.style.display 			= 'none';
-	vote_btn_contra.style.display 		= 'none';
-	vote_btn_none.style.display 		= 'none';
-	vote_btn_skip.style.display 		= 'none';
-	prev_btn.style.display 				= 'none';
-	show_opinion.style.display 			= 'none';
-	show_big_parties.style.display 		= 'none';
-	show_secular_parties.style.display 	= 'none';
-	opinion_div.style.display 			= 'none';
-	done_btn.style.display 				= 'none';
-	weighted_div.style.display 			= 'none';
-	restart_btn.style.display 			= 'none';
+	vote_btn_pro.style.display = 'none';
+	vote_btn_contra.style.display = 'none';
+	vote_btn_none.style.display = 'none';
+	vote_btn_skip.style.display = 'none';
+	prev_btn.style.display = 'none';
+	show_opinion.style.display = 'none';
+	show_big_parties.style.display = 'none';
+	show_secular_parties.style.display = 'none';
+	opinion_div.style.display = 'none';
+	done_btn.style.display = 'none';
+	weighted_div.style.display = 'none';
+	restart_btn.style.display = 'none';
 
 	// Set events
 	start_btn.onclick = () => {
 		// Set display
-		start_btn.style.display 			= 'none';
-		vote_btn_pro.style.display 			= 'inline-block';
-		vote_btn_contra.style.display 		= 'inline-block';
-		vote_btn_none.style.display 		= 'inline-block';
-		vote_btn_skip.style.display 		= 'inline-block';
-		prev_btn.style.display 				= 'inline-block';
-		show_opinion.style.display 			= 'inline-block';
-		// show_big_parties.style.display 		= 'inline-block';
-		// show_secular_parties.style.display 	= 'inline-block';
+		start_btn.style.display = 'none';
+		vote_btn_pro.style.display = 'inline-block';
+		vote_btn_contra.style.display = 'inline-block';
+		vote_btn_none.style.display = 'inline-block';
+		vote_btn_skip.style.display = 'inline-block';
+		prev_btn.style.display = 'inline-block';
+		show_opinion.style.display = 'inline-block';
 
 		// Fill party votes
 		for(let i = 0; i < parties.length; i++){
 			party_vote_count[parties[i].name] = 0;
 		}
-
 		// Call functions
-		loadQuestion(question);
+		loadQuestion(question);/
 	};
 
 	prev_btn.onclick = () => {
@@ -76,13 +73,13 @@ let Stemwijzer = (function(){
 		// Set opinion text
 		for(let i = 0; i < subjects[question]['parties'].length; i++){
 
-			let name 	= document.createElement('h2');
-			let vote 	= document.createElement('h5');
+			let name = document.createElement('h2');
+			let vote = document.createElement('h5');
 			let opinion = document.createElement('p');
-			let streep 	= document.createElement('hr');
+			let streep = document.createElement('hr');
 
-			name.innerHTML 		= subjects[question]['parties'][i].name;
-			opinion.innerHTML 	= subjects[question]['parties'][i].opinion;
+			name.innerHTML = subjects[question]['parties'][i].name;
+			opinion.innerHTML = subjects[question]['parties'][i].opinion;
 
 			// Translate vote
 			let translated = '';
@@ -161,23 +158,23 @@ let Stemwijzer = (function(){
 	function loadQuestion(q){
 		if(q < subjects.length){
 			// Load new question
-			title_div.innerHTML 	= (question+1) + '. ' + subjects[q]['title'];
+			title_div.innerHTML = (question+1) + '. ' + subjects[q]['title'];
 			statement_div.innerHTML = '"'+subjects[q]['statement']+'"';
 		}
 		else {
 			// End of questions
-			title_div.innerHTML 	= 'Kies de vragen die zwaar moeten mee tellen.';
+			title_div.innerHTML = 'Kies de vragen die zwaar moeten mee tellen.';
 			statement_div.innerHTML = '';
 
-			vote_btn_pro.style.display 			= 'none';
-			vote_btn_contra.style.display 		= 'none';
-			vote_btn_none.style.display 		= 'none';
-			vote_btn_skip.style.display 		= 'none';
-			prev_btn.style.display 				= 'none';
-			show_opinion.style.display 			= 'none';
-			show_big_parties.style.display 		= 'none';
-			show_secular_parties.style.display 	= 'none';
-			done_btn.style.display 				= 'inline-block';
+			vote_btn_pro.style.display = 'none';
+			vote_btn_contra.style.display = 'none';
+			vote_btn_none.style.display = 'none';
+			vote_btn_skip.style.display = 'none';
+			prev_btn.style.display = 'none';
+			show_opinion.style.display = 'none';
+			show_big_parties.style.display = 'none';
+			show_secular_parties.style.display = 'none';
+			done_btn.style.display = 'inline-block';
 			loadWeightedQuestions();
 		}
 	}
@@ -225,11 +222,11 @@ let Stemwijzer = (function(){
 
 	function showResults(vote_count){
 		// Set display
-		weighted_div.style.display 	= 'none';
-		done_btn.style.display 		= 'none';
-		restart_btn.style.display 	= 'inline-block';
+		weighted_div.style.display = 'none';
+		done_btn.style.display = 'none';
+		restart_btn.style.display = 'inline-block';
 
-		title_div.innerHTML 		= 'Uw resultaat:';
+		title_div.innerHTML = 'Uw resultaat:';
 
 		sorted_keys = Object.entries(vote_count).sort((a, b) => b[1] - a[1]).map(el => el[0]);
 
